857ed61d3474b7192a3f33f07499562da7be8f6c
Andrew Mayer <62214215+computer-lov@users.noreply.github.com>
2021-07-01 13:52:59 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from cortex import Cortex
 import webbrowser
 from pynput.mouse import Button, Controller
 import time
+import subprocess
 
 # This is the main file that will execute the tic-tac-toe game 
-# try to finish this by end of next week 7/1
 
 # TODO
-# NEED TO WRITE CODE TO ENABLE LEFT-CLICK DURING GAME PLAY
+# is it possible to use both the mental commands and the facial expressions in the same application?
 # FIRST PLAY GAME AGAINST COMPUTER
 # NEXT TRY TO PLAY 2-PLAYER VERSION OF GAME PROVIDED ON WEBSITE WITH TWO EPOC FLEX DEVICES!
 
@@ -15,26 +15,20 @@ import time
 class userSession():
     def __init__(self):
         user = {'client_id': 'BN6wnwY8b9ZKYAQmTUCJLHBx0UVQ1VE52QN4I9Ha', 'client_secret' : 'WSdbaAxrMqkNvqRvMYW8ZsLXWNuNb3XJGk4cnxXebQb3A43bl7L21AEvr7aiQqOepIo01K74ixfDSKPb1QBhUPPX9EOewegV4kYZCJceDiGBZFfAKrSN5MIpTQroOhg6', 
-        'license' : 'd5b584b8-883e-421f-8bf5-cbe4bcb0ac72', 'debit' : 0}
+        'license' : 'd5b584b8-883e-421f-8bf5-cbe4bcb0ac72', 'debit' : 100}
         self.userSession = Cortex(user)
         self.mouse = Controller()
 
     # creates a session for the user
     def openSession(self):
         # request access / query headset / control device / authorize / create session
-           self.userSession.do_prepare_steps()
+        self.userSession.do_prepare_steps()
 
     # open website where tictactoe game will be played
     def openWebsite(self):
         self.mouse.position = (1277, 672)
         webbrowser.open('https://playtictactoe.org/')
-
-    # starts live session
-    def goLive(self, profile_name):
-        # go live
-        self.userSession.setup_profile(profile_name, status='load')
-        self.userSession.sub_request(stream=['com'])
-        
+   
     # moves the mouse to the left
     def moveMouseLeft(self):
         self.mouse.move(-205, 0)
@@ -64,21 +58,32 @@ class userSession():
         self.mouse.press(Button.left)
         self.mouse.release(Button.left)
 
-    def determineAction(self):
-        desiredAction = self.userSession.get_mental_command_active_action()
-    
-        for action in desiredAction['result']:
-            if action == "push":
-                self.moveMouseLeft()
-            elif action == "pull":
-                self.moveMouseRight()
-            elif action == "lift":
-                self.moveMouseUp()
-            elif action == "drop":
-                self.mouseMouseDown()
-            # else:  neutral do nothing
+    def determineAction(self, output):
+        action = "neutral"
+        desiredAction = str(output.strip())
+        if "com" and "sid" and "time" in desiredAction:
+            dataParse = desiredAction.split('"')
+            action = dataParse[3]
+        return action
 
-        # need to write code to determine left-click action
+    def executeAction(self, action):
+        print("Execute: ", action)
+        # go left
+        if action == "left":
+            self.moveMouseLeft()
+        # go right
+        elif action == "right":
+            self.moveMouseRight()
+        # go up
+        elif action == "lift":
+            self.moveMouseUp()
+        # go down
+        elif action == "drop":
+            self.moveMouseDown()
+        # left click on mouse
+        elif action == "push":
+            self.leftClickMouse()
+        # else: neutral do nothing
 
     def close(self):
         self.userSession.disconnect_headset()
@@ -88,17 +93,39 @@ class userSession():
 if __name__ == '__main__':
     openSession = userSession()
     openSession.openSession()
-    openSession.openWebsite()
-    openSession.goLive('EmotivBCI-Andrew Paul Mayer')
-    cont = True;
+    # openSession.openWebsite()
+    cont = True
     start = time.time()
+    # runs live_advance.py file
+    process = subprocess.Popen('live_advance.py', shell = True, stdout=subprocess.PIPE)
+    count = 0
+    actionList = []
     while cont:
-        openSession.determineAction()
         end = time.time() - start
-        if end >= 60:
+        # collect each line of stdout from live_advance.py
+        output = process.stdout.readline()
+        time.sleep(0.1)
+        if process.poll() is not None and output == '':
             cont = False
+        if end >= 300: # if time > 5 mins
+            cont = False
+        if output:
+            # determines action to be taken
+            action = openSession.determineAction(output)
+            # keeps track of amount of times action has occured
+            actionList.append(action)
+            if count == 0:
+                count += 1
+            elif actionList[count] == actionList[count-1]:
+                count += 1
+                # if it occurs over 10 times execute action
+                if count >= 10:
+                    count = 0
+                    actionList.clear()
+                    openSession.executeAction(action)
+            else: # actionList[count] != actionList[count=1]
+                count = 0
+                actionList.clear()
 
+    retval = process.poll()
     openSession.close()
-
-
-
