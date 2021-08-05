78debb3d5d77a18e0ba0e538c6fded25fd7e3800
Andrew Mayer <62214215+computer-lov@users.noreply.github.com>
2021-08-05 13:43:25 -0400

    Update userSession.py

--- userSession.py
+++ userSession.py
@@ -2,14 +2,14 @@ from cortex import Cortex
 import webbrowser
 import subprocess
 
-# Controls mouse on screen
+# creates Emotiv session for Pursuit Evasion Game
 
 class userSession():
     def __init__(self):
-        user = {'client_id': 'BN6wnwY8b9ZKYAQmTUCJLHBx0UVQ1VE52QN4I9Ha', 'client_secret' : 'WSdbaAxrMqkNvqRvMYW8ZsLXWNuNb3XJGk4cnxXebQb3A43bl7L21AEvr7aiQqOepIo01K74ixfDSKPb1QBhUPPX9EOewegV4kYZCJceDiGBZFfAKrSN5MIpTQroOhg6', 
-        'license' : 'd5b584b8-883e-421f-8bf5-cbe4bcb0ac72', 'debit' : 100}
+        user = {'client_id': '', 'client_secret' : '', 
+        'license' : '', 'debit' : 100}
         self.userSession = Cortex(user)
-         # runs live_advance.py file
+        # runs live_advance.py file
         self.process = subprocess.Popen("live_advance.py", shell = True, stdout=subprocess.PIPE)
 
     # creates a session for the user
@@ -50,8 +50,8 @@ class userSession():
     # records eeg signals during session
     def beginRecord(self):
         # record parameters
-        recordName = "Pursuit Evasion Trials"
-        recordDescription = "Pursuit Evasion 25 Trials for Undergraduate Summer Research Project"
+        recordName = 'Pursuit Evasion Trials'
+        recordDescription = 'Pursuit Evasion 25 Trials for Undergraduate Summer Research Project'
         # start record --> stop record --> disconnect headset --> export record
         self.userSession.create_record(recordName, recordDescription)
 
@@ -63,7 +63,7 @@ class userSession():
     # exports the recording
     def exportRecord(self):
         # export parameters
-        recordExportFolder = "C:/Users/larx/Desktop/USRP Project 2021/EEGRecordings"
+        recordExportFolder = ''
         recordExportDataTypes = ['EEG', 'MOTION', 'PM', 'BP']
         recordExportFormat = 'EDF'
         recordExportVersion = 'V2'
