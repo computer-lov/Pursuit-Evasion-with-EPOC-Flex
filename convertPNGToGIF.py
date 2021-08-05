a1ef5c31bb6b569b0cd3ecfd77b894cdef3365fc
Andrew Mayer <62214215+computer-lov@users.noreply.github.com>
2021-08-05 13:40:04 -0400

    Update convertPNGToGIF.py

--- convertPNGToGIF.py
+++ convertPNGToGIF.py
@@ -3,18 +3,16 @@ from PIL import Image
 # program to convert multiple .png files to a single .gif file
 
 if __name__ == "__main__":
-    for num in range(1,26):
-        # Create the frames
-        frames = []
-    
-        for i in range(50):
-            file = 'trialResult' + str(num) + '_chunck' + str(i) + '.png'
-            newFrame = Image.open(file)
-            frames.append(newFrame)
+    pngFiles = 5
+    frames = []
+    for i in range(pngFiles):
+        file = 'fileName' + str(i) + '.png'
+        newFrame = Image.open(file)
+        frames.append(newFrame)
 
-        # Save into a GIF file that loops forever
-        saveFile = 'trialResult' + str(num) + '.gif'
-        frames[0].save(saveFile, format='GIF',
-                    append_images=frames[1:],
-                    save_all=True,
-                    duration=300, loop=0)
+    # Save into a GIF file that loops forever
+    saveFile = 'saveFileName' + '.gif'
+    frames[0].save(saveFile, format='GIF',
+                append_images=frames[1:],
+                save_all=True,
+                duration=300, loop=0)
