7fbba4f44ea3acfcbcdd4ca0dcbc4f385d237bfe
Andrew Mayer <62214215+computer-lov@users.noreply.github.com>
2021-08-05 13:15:29 -0400

    Add files via upload

---
+++ convertPNGToGIF.py
@@ -0,0 +1,18 @@
+from PIL import Image
+ 
+if __name__ == "__main__":
+    for num in range(1,26):
+        # Create the frames
+        frames = []
+    
+        for i in range(50):
+            file = 'trialResult' + str(num) + '_chunck' + str(i) + '.png'
+            newFrame = Image.open(file)
+            frames.append(newFrame)
+
+        # Save into a GIF file that loops forever
+        saveFile = 'trialResult' + str(num) + '.gif'
+        frames[0].save(saveFile, format='GIF',
+                    append_images=frames[1:],
+                    save_all=True,
+                    duration=300, loop=0)
